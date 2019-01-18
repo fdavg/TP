17e73e5835f1221a45250633abe0e415c35c41d3
DAVID GNANHOUI Franck <david_gnanhoui.gildas_franck@courrier.uqam.ca>
2019-01-18 01:33:29 -0500

    Mise a jour du fichier programme  à 200

--- Principale.java
+++ Principale.java
@@ -4,7 +4,7 @@ public class Principale {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		for (int i = 1; i <= 100; i++) {
+		for (int i = 1; i <= 200; i++) {
 			if (i % 3 != 0 && i % 5 != 0) {
 				System.out.println(i);
 			} else if (i % 3 == 0 && i % 5 != 0) {
